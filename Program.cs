dd64da836510e80becc4290bd7037610676631ff
SuciuAndrea <146415039+SuciuAndrea@users.noreply.github.com>
2023-10-16 19:04:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ParSauImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("par");
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
+        }
+    }
+}
